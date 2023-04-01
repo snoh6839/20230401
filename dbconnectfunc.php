8b0bb59d2dc6ddebcaa2a9853ae925dc857496ef
SubinNoh <snoh6839@mysvc.skagit.edu>
2023-04-01 22:14:32 +0900

    update

--- dbconnectfunc.php
+++ dbconnectfunc.php
@@ -1,38 +1,31 @@
 <?php
+
+//함수만들기
 function execute_query($sql)
 {
-    // connect to db
     $dbc = mysqli_connect("localhost", "root", "root506", "employees", 3306);
-
-    // execute query
     $result = mysqli_query($dbc, $sql);
 
-    // check if there are any results
     if (mysqli_num_rows($result) > 0) {
-        // create an array to store the results
         $rows = array();
         while ($row = mysqli_fetch_assoc($result)) {
             $rows[] = $row;
         }
-        // close db connection
+
         mysqli_close($dbc);
 
-        // return the results
         return $rows;
     } else {
-        // close db connection
         mysqli_close($dbc);
 
-        // return 0 if no results found
         return 0;
     }
 }
 
-// execute query
+// 함수 사용 예시
 $sql = "SELECT emp_no, concat(last_name, ' ' , first_name) as full_name, gender, birth_date FROM employees Where emp_no <= 10005;";
 $result = execute_query($sql);
 
-// output results
 if ($result) {
     foreach ($result as $row) {
         echo "emp_no: " . $row["emp_no"] . " full_name: " . $row["full_name"] . " gender: " . $row["gender"] . " birth_date: " . $row["birth_date"] . "\n";
